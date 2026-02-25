573a6be8b56bed7fba9173ff503eb7c5af087246
RodrigoGit87 <rodrigollamas87@gmail.com>
2026-02-26 00:47:46 +0100

    Added a minor task for clarifying loops

--- Module 2/Bucles.py
+++ Module 2/Bucles.py
@@ -1,3 +1,5 @@
+import random
+
 #FOR
 # Un bucle for itera sobre una secuencia, como una lista, tupla o cadena, ejecutando un bloque de código por cada 
 # elemento de la secuencia. Esto resulta especialmente útil cuando se trabaja con colecciones de datos 
@@ -19,4 +21,22 @@ while not valid_input:
   else:
     print("Invalid input. Please try again.")
 
+#TAREA
+#Creará un juego de adivinar números en el que el ordenador intentará adivinar un número secreto que usted le indique.
+#El ordenador generará conjeturas aleatorias dentro de un rango (1 a 10) y continuará adivinando hasta que encuentre el número correcto
+
+#1.onfigure su secret_number: Elija un número entre 1 y 10 y asígnele una variable llamada secret_number
+#2.Inicializa otra variable llamada guess con un valor de 0.
+#3.Completa el bucle while: Añada la condición al bucle while para asegurarse de que continúa ejecutándose mientras guess no sea igual a su secret_number.
+
+
+# Set the secret_number variable here (between 1 and 10)
+secret_number = 3
+# Initialize the guess variable here with a value of 0
+guess = 0
+while not guess == secret_number:
+	guess = random.randint(1, 10)
+	print("Guessing:",guess)
+
+print("I guessed the right number! It was",secret_number)
 
