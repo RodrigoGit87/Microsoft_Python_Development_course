b5a6ad3bc1998fbf33ce53408b79158aa7df67d7
RodrigoGit87 <rodrigollamas87@gmail.com>
2026-03-01 16:24:06 +0100

    "format code"

--- Module 2 Basic Concepts/Bucles.py
+++ Module 2 Basic Concepts/Bucles.py
@@ -1,12 +1,12 @@
 import random
 
-#FOR
+#---- FOR -------
 # Un bucle for itera sobre una secuencia, como una lista, tupla o cadena, ejecutando un bloque de código por cada 
 # elemento de la secuencia. Esto resulta especialmente útil cuando se trabaja con colecciones de datos 
 # o cuando se conoce de antemano el número de iteraciones.
 for i in range(0,10):
     print("iteration:",i)
-#ITERACIONES POSIBLES EN FOR
+# ITERACIONES POSIBLES EN FOR
 # range(stop): Genera una secuencia de números empezando por 0 (por defecto) hasta, pero sin incluir, el valor stop, incrementándose en 1 cada vez.
 # Así, range(5) haría un bucle del 0 al 4. -- No hay homologo en Java --
 
@@ -16,9 +16,9 @@ for i in range(0,10):
 # range(start, stop, step): Esta es la forma más flexible, ya que permite definir el incremento (o decremento) entre los números de la secuencia. 
 # Genera números empezando por el inicio, continuando hasta (pero sin incluir) el final, con cada número incrementado (o decrementado si el paso es negativo) por paso.
 # Así, range(1, 10, 2) genera 1, 3, 5, 7 y 9. Una versión que utiliza pasos negativos es range(10, 5, -1), que generaría 10, 9, 8, 7 y 6.
-# En Java: for (int i=1; i <= 10; i++){...}
+# En Java: for (int i=1; i < 10; i++){...}
 
-#WHILE
+#------ WHILE --------
 #Un bucle while, por el contrario, ejecuta un bloque de código mientras se cumpla una condición especificada.
 #Esto resulta útil cuando se desconoce de antemano el número de iteraciones o cuando el bucle debe continuar 
 #hasta que se cumpla una condición específica
@@ -31,7 +31,7 @@ while not valid_input:
   else:
     print("Invalid input. Please try again.")
 
-#SENTENCIAS BREAK Y CONTINUE
+#--- SENTENCIAS BREAK Y CONTINUE ---
 #Se usa break para interrumpir y salir del bucle
 text = "OpenEDG Python Institute"
 for letter in text:
@@ -46,11 +46,9 @@ for letter in text:
         continue
     print(letter, end="")
 
-
-#TAREA
+#-- TAREA --
 #Creará un juego de adivinar números en el que el ordenador intentará adivinar un número secreto que usted le indique.
 #El ordenador generará conjeturas aleatorias dentro de un rango (1 a 10) y continuará adivinando hasta que encuentre el número correcto
-
 #1.onfigure su secret_number: Elija un número entre 1 y 10 y asígnele una variable llamada secret_number
 #2.Inicializa otra variable llamada guess con un valor de 0.
 #3.Completa el bucle while: Añada la condición al bucle while para asegurarse de que continúa ejecutándose mientras guess no sea igual a su secret_number.
@@ -66,7 +64,7 @@ while not guess == secret_number:
 
 print("I guessed the right number! It was",secret_number)
 
-#LISTAS 
+#------ LISTAS ------- 
 #Es una estructura de datos que almacena una colección ordenada de elementos. La diferencia con el Array es que los elementos pueden ser de cualquier tipo dentro de la lista.
 #Las listas son dinamicas, y lso arrays son de tamaño fijo. Lo mas parecido en Java es el ArrayList o List<>, pero Java si obliga a que se un determinado tipo de dato.
 #Las listas se definen encerrando los elementos entre corchetes [], separados por comas.
@@ -86,11 +84,11 @@ students = ["Alice", "Bob", "Charlie"]
 for i in range(0,len(students)):
     print("Hello,", students[i]) 
 
-#TAREA
+# TAREA
 # Crear un bucle for: Utilice la función range() para generar la secuencia de números. Recuerde que range(max_value + 1) incluirá max_value en el bucle.
 
 # Comprueba la divisibilidad: Dentro del bucle for, utilice una sentencia if para comprobar si el número actual es divisible entre 3 y 4. 
-# Para ello, utilice el operador módulo (%). Para ello, utilice el operador de módulo (%) para encontrar el resto al dividir por 3, y de nuevo al dividir por 4.
+# Para ello, utilice el operador de módulo (%) para encontrar el resto al dividir por 3, y de nuevo al dividir por 4.
 #  Si ambos restos son 0, utilice print() para mostrar el número.
 max_value = 50
 
